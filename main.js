e9353c1a1231760bb4449a59dda93300c7195d03
Michał <michal_slezak@icloud.com>
2021-03-11 01:39:40 +0100

    CSS normalization added

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ console.log('Siema');
 
 const textByJs = document.querySelector('.textByJs');
 textByJs.innerHTML = "Tekst dodany przez JS";
+
+const calculating = (a,b) => a*b;
+
+console.log(calculating(3, 4));
+
+
+
+
