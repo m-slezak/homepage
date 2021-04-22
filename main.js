fd55b7787945459701540e4668cb98422c00c75e
Michał <michal_slezak@icloud.com>
2021-04-22 18:34:44 +0200

    Hamburger menu

--- main.js
+++ main.js
@@ -2,9 +2,29 @@ console.log('Siema');
 
 
 
-const greetings = (name, age) => console.log('${age}, ${name}');
-const name = "Janusz";
-const age = 23;
+const greeting = (name, age) => {
+    console.log(`Witaj Drogi Odwiedzający mam ${age} lata i nazywam się ${name}`);
+}
+
+greeting ('Michał', 36);
+
+
+console.log(typeof 2);
+console.log(typeof '2');
+if (2 === '2') {
+    console.log ('Tak to prawda');
+}
+
+
+const hamburger = document.querySelector(".hamburger--js")
+
+hamburger.addEventListener('click', () =>{
+ const nav = document.querySelector('.navigation--js');
+ nav.classList.toggle('navigation--open');
+
+})
+
+
 
 
 
